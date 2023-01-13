58f1e5440fd847e8f64712779a30b57bba9695b3
laxmikantkapse <105602990+laxmikantkapse@users.noreply.github.com>
2023-01-13 11:56:56 +0530

    Delete validatable.rb

--- models/concerns/validatable.rb
+++
@@ -1,8 +0,0 @@
-module Validatable
-  extend ActiveSupport::Concern
-
-  included do 
-    validates :name, presence: true, uniqueness: true
-    validates :description, presence: true, length: { minimum: 10, maximum: 1000 }
-  end
-end
